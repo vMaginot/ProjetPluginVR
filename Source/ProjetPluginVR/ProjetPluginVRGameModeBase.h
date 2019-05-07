29500183c5aa01ca3d3af829b95a1ca601ea9510
Vincent <vincent.maginot@gmail.com>
2019-05-07 14:56:38 +0200

    Initial commit

---
+++ Source/ProjetPluginVR/ProjetPluginVRGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjetPluginVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJETPLUGINVR_API AProjetPluginVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
